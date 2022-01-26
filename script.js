8c7cfc99add9164a9ee5f56a395d66121cc00cd2
Lucca Rendall <luccarendall1@gmail.com>
2022-01-26 11:16:06 -0300

    Correçao req. 2

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
+// Constantes de seleção de elemento (querySelector ou getElement)
 const colors = document.querySelectorAll('.color');
+const pixelBoard = document.getElementById('pixel-board')
 
 // Requisito 7 - Clicar em uma das cores da paleta faz com que ela seja selecionada e utilizada para preencher os pixels no quadro.
  
@@ -11,11 +13,6 @@ colors.forEach((color) => color.addEventListener('click', removeSelectedColor));
 
 // 8 - Clicar em um pixel dentro do quadro após selecionar uma cor na paleta faz com que o pixel seja preenchido com a cor selecionada.
 
+//Pegar o evento de click com o target e capturar uma parte do html para editar (mudando a posição do select), fazer isso com innerHTML
+
 // Requisito 9 - Crie um botão que, ao ser clicado, limpa o quadro preenchendo a cor de todos seus pixels com branco.
-
-function resetColors(e) {
-  colors.forEach((color) => color.classList.add('.white'));
-  e.target.classList.add('selected');
-}
-
-colors.forEach((color) => color.addEventListener('click', resetColors));
