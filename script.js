018da4eb87c230ce9791877c9f737da35819d313
Lucca Rendall <luccarendall1@gmail.com>
2022-01-31 02:47:00 -0300

    Requisito 9

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 const colors = document.querySelectorAll('.color');
 const pixelBoard = document.getElementById('pixel-board')
 const btnClearBoard = document.getElementById('clear-board');
-
+const vqvBtn = document.getElementById('generate-board');
 // Requisito 7 - Clicar em uma das cores da paleta faz com que ela seja selecionada e utilizada para preencher os pixels no quadro.
  
 function removeSelectedColor(e) {
@@ -29,20 +29,19 @@ function selectColor (event) {
 //Pegar o evento de click com o target e capturar uma parte do html para editar (mudando a posição do select), fazer isso com innerHTML
 
 // Requisito 9 - Crie um botão que, ao ser clicado, limpa o quadro preenchendo a cor de todos seus pixels com branco.
-
+//Se liga nessa gambiarra
 const clearBtn = document.getElementById('clear-board');
-
-
-function clear() {
-  const pixel = document.getElementById('pixel');
-  console.log(pixel);
-  for (i = 0; i < pixel.length; i += 1) {
-    pixel[i].style.backgroundColor = 'white';
-  }
+function clear(event) { 
+  location. reload();
 }
+clearBtn.addEventListener('click', clear); 
+// clearBtn.addEventListener('click', clear); //Esse estudor é acionado ao clicar no botão Limpar e executa a função clear
 
-clearBtn.addEventListener('click', clear);
-// Requisito 10 - Faça o quadro de pixels ter seu tamanho definido pela pessoa usuária. 
+// Requisito 10 - Faça o quadro de pixels ter seu tamanho definido pela pessoa usuária.
+// function tamanhoBoard() {
+// console.log('Cliquei aqui')
+// }
+// vqvBtn.addEventListener('click', tamanhoBoard)
 // Requisito 11 - Limite o tamanho mínimo e máximo do board.
 
 // Requisito 12 - Faça com que as cores da paleta sejam geradas aleatoriamente ao carregar a página.
